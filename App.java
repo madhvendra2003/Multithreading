f386892f7f16da500f0c7ec867b11507391e5763
Madhvendra Singh <madvendra1789@gmail.com>
2024-10-12 19:53:01 +0530

    new threwad was created

--- App.java
+++ App.java
@@ -2,8 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
          
-        Thread thread1 = new Thread1();
+        Thread thread1 = new Thread1();       
         thread1.start();
+
+        Thread thread = new Thread(new Thread2(),"Thread2");
+        thread.start();
+
 
         System.out.println("main is ending");
 
