7a7aaf11918feec54bf5d89499e513c923b953ac
Madhvendra Singh <madvendra1789@gmail.com>
2024-10-12 19:36:31 +0530

    Creation of thread

--- App.java
+++ App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+         
+        Thread thread1 = new Thread1();
+        thread1.start();
+
+        System.out.println("main is ending");
+
     }
 }
